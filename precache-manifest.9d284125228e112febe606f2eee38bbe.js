9968c236a56ca37e0b0daeb4f6dd2904ba27555a
Sean Bannon <seannbannon@gmail.com>
2021-07-12 18:26:20 -0500

    Updates

---
+++ precache-manifest.9d284125228e112febe606f2eee38bbe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e080eb1eae4066b43fceed256a5531",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "124310024c5a7d705e18",
+    "url": "/react_portfolio/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "826be098bfad87470631",
+    "url": "/react_portfolio/static/css/main.93805639.chunk.css"
+  },
+  {
+    "revision": "124310024c5a7d705e18",
+    "url": "/react_portfolio/static/js/2.021e9159.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react_portfolio/static/js/2.021e9159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "826be098bfad87470631",
+    "url": "/react_portfolio/static/js/main.b44085b2.chunk.js"
+  },
+  {
+    "revision": "5397e927921dde71372c",
+    "url": "/react_portfolio/static/js/runtime-main.d05fa38a.js"
+  },
+  {
+    "revision": "aad65f6c748d50e4241d7046cd4a6bfe",
+    "url": "/react_portfolio/static/media/budget.aad65f6c.png"
+  },
+  {
+    "revision": "3fb960585adc1379d7830b9ce6ad4a11",
+    "url": "/react_portfolio/static/media/dayplanner.3fb96058.png"
+  },
+  {
+    "revision": "36a9151820da4a78a6b1dd1ba1ec14d3",
+    "url": "/react_portfolio/static/media/me.36a91518.JPG"
+  },
+  {
+    "revision": "6a9babf3038011f198d9e97f9e54a3fb",
+    "url": "/react_portfolio/static/media/spaceinvaders.6a9babf3.png"
+  },
+  {
+    "revision": "758eb27731257ade9778da0f9f837b4f",
+    "url": "/react_portfolio/static/media/takeahike.758eb277.png"
+  },
+  {
+    "revision": "8e624443e759b1cce2242ec508dbff7e",
+    "url": "/react_portfolio/static/media/vintage.8e624443.png"
+  },
+  {
+    "revision": "055d9184a7d0d9a12d75fcc7563f9c0a",
+    "url": "/react_portfolio/static/media/weather.055d9184.png"
+  }
+]);
